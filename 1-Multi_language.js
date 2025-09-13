5cfa01b9c4d8c3bc0c395fda9cb5baf0c6796f29
abonilehoofman <abonilehoofman.a@gmail.com>
2025-09-13 23:58:52 +0200

    Create 1-Multi_language.js

---
+++ 1-Multi_language.js
@@ -0,0 +1,6 @@
+const messages=[ "C is fun",
+"Python is cool",   
+"Javascript is amazing"
+];
+messages,forEach(msg=>console.log(msg))
+console.log("C is fun\nPython is cool\nJavascript is amazing");
