c540ee824f08bb3f943ccaf390e29c7cc59cabd9
abonilehoofman <abonilehoofman.a@gmail.com>
2025-09-14 15:37:38 +0200

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 const arg = process.argv[2];
 if ( arg && !isNaN(Number(arg))){
-  console.log( "My number:" +parseIt(arg));
+  console.log( "My number:" +parseInt(arg));
 }else{
   console.log("Not a number");
 }
